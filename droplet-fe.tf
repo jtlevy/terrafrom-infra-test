529db4c62b8ec1603849df8bc47da47211a94bfc
Shua <josh@traverstodd.com>
2021-01-28 12:19:31 -0800

    updated user_data

--- droplet-fe.tf
+++ droplet-fe.tf
@@ -15,33 +15,13 @@ resource "digitalocean_droplet" "merik-fe" {
 
 	private_networking = true
 
-		user_data = <<EOF
-		#cloud-config
-		package_upgrade: true
-		runcmd:
-		# Apache
-		- sudo apt update -y
-		- sudo apt install apache2 -y
-
-		- sudo a2enmod rewrite
-		- sudo a2enmod ssl
-		- sudo a2ensite default-ssl.conf
-		- sudo service apache2 restart
-		
-		# PHP7.4
-		- sudo apt-get update
-		- sudo apt install software-properties-common -y
-		- sudo add-apt-repository ppa:ondrej/php -y
-		- sudo apt-get update
-
-		# PHP - this installs 7.4
-		- sudo apt install php7.4 libapache2-mod-php -y
-		- sudo apt install php-curl php-gd php-mbstring php-xml php-xmlrpc php-intl php-zip -y
-		
-		# checks for syntax errors in apache conf
-		- sudo apache2ctl configtest
-		- sudo systemctl restart apache2
-	EOF
+	user_data = <<EOF
+    #cloud-config
+    packages:
+        - nginx
+    runcmd:
+        - mkdir ~/terraWorked
+    EOF
 
 	lifecycle {
 		create_before_destroy = true // dont destroy server before the new one is running!
